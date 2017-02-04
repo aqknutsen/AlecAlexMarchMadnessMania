752a1865a7631d3d2649f07aef85370cf8278f27
Alec Knutsen <aqknutsen@gmail.com>
2017-02-03 21:39:21 -0600

    Add queries

--- GetData.py
+++ GetData.py
@@ -8,63 +8,113 @@ class GetData:
     def __init__(self):
         pass
 
+
+
+    create_table_queries = ['''create table EspnScoringData
+                               (YEAR integer, TEAM text, GP integer, PTS numeric, FGMvsFGA text, FGPer numeric, THRMvsTHRAA text
+                              , THRPer numeric, FTMvsFTMA text, FTPer numeric)''',
+                            '''create table EspnReboundData
+                               (YEAR integer, TEAM text, GP integer, OFF integer, ORPG numeric, DEF integer, DRPG numeric
+                              , REB integer, RPG numberic)''',
+                            '''create table EspnFieldGoalData
+                              (YEAR integer, TEAM text, GP integer, PPG numeric, FGMPerGame numeric, FGAPerGame numeric, FGM integer
+                             , FGA integer, FGPer numeric, TwoPointersMade integer, TwoPointersAttempted integer,
+                              TwoPointPercentage integer, PPS numeric, AdjustedFieldGoalPer numeric )''',
+                            '''create table EspnFreeThrows
+                              (YEAR integer, TEAM text, GP integer, PPG numeric, FTMPer numeric, FTAPer numeric, FTM integer
+                             , FTA integer, FTPercentage numeric )''',
+                            '''create table ThreePointData
+                                 (YEAR integer, TEAM text, GP integer, PPG numeric, ThreesMadePerGame numeric, ThreesAttemptedPerGame numeric, ThreesMade integer
+                                , ThreesAttempted integer, ThreePercentage numeric, TwoPointersMade integer, TwoPointersAttempted integer,
+                                 TwoPointPercentage integer, PPS numeric, AdjustedFieldGoalPer numeric )''',
+                            '''create table EspnAssists
+                                  (YEAR integer, TEAM text, GP integer, AST integer, APG numeric, TO integer, TOPG numeric
+                                 , AssistTurnoverRatio numeric)''',
+                            '''create table EspnSteals
+                                  (YEAR integer, TEAM text, GP integer, STL integer, STPG numeric, TO integer, TOPG numeric
+                                 , PF integer, StealsTurnoverRation numeric, StealsPersonalFouls numeric)''',
+                            '''create table EspnBlocks
+                                  (YEAR integer, TEAM text, GP integer, BLK integer, PF integer, BLKPG numeric, BLKTOPF numeric)''',
+
+    ]
+
+    insert_queries = ['insert into ScoringData values (?,?,?,?,?,?,?,?,?,?)',
+                      'insert into EspnReboundData values (?,?,?,?,?,?,?,?,?)',
+                      'insert into EspnFieldGoalData values (?,?,?,?,?,?,?,?,?, ?, ?, ?, ?, ?)',
+                      'insert into EspnFreeThrows values (?,?,?,?,?,?,?,?,?)',
+                      'insert into ThreePointData values (?,?,?,?,?,?,?,?,?, ?, ?, ?, ?, ?)',
+                      'insert into EspnAssists values (?,?,?,?,?,?,?,?)',
+                      'insert into EspnSteals values (?,?,?,?,?,?,?,?,?,?)',
+                      'insert into EspnBlocks values (?,?,?,?,?,?,?)'
+    ]
+
+    statslist = ['scoring-per-game', 'rebounds', 'field-goals', 'free-throws', '3-points', 'assists', 'steals',
+                 'blocks']
+
+    lengths = [9, 10, 14, 9, 14, 8, 10, 7]
+
+    def espnpulls(self):
+        fulllist = []
+        print 'hello'
+
+        for year in range(2002, 2017):
+            for statitem in self.statslist:
+                for count in range(1, 360, 40):
+                    fulllist.append(
+                        'http://www.espn.com/mens-college-basketball/statistics/team/_/stat/' + statitem + '/year/' + str(
+                            year) + '/count/' + str(count))
+        return fullist
+
     def get_scores(self):
 
-        year = 2017
-
+        url_links = espnpulls
 
         conn = sqlite3.connect('ESPNDATA.db')
         c = conn.cursor()
-        c.execute('''create table ScoringData
-                               (YEAR integer, TEAM text, GP integer, PTS numeric, FGMvsFGA text, FGPer numeric, THRMvsTHRAA text
-                              , THRPer numeric, FTMvsFTMA text, FTPer numeric)''')
-
-        while year != 2002:
-
-            count = 1
-            scoring_data = [[]]
-
-            while count <= 351:
-
-                if year != 2017:
-                    url = 'http://www.espn.com/mens-college-basketball/statistics/team/_' \
-                          '/stat/scoring-per-game/sort/avgPoints/year/'+str(year)+'/count/' + str(count)
-                else:
-                    url = 'http://www.espn.com/mens-college-basketball/statistics/team/_' \
-                          '/stat/scoring-per-game/sort/avgPoints/count/' + str(count)
-
-                request = Request(url)
-
-                try:
-                    response = urlopen(request)
-                    soup = BeautifulSoup(response.read(), 'html.parser')
-
-                    for link in soup.find_all('a'):
-                        if 'mens-college-basketball/team/' in link.get('href'):
-                            row = link.find_parent('tr')
-                            temp_list = []
-                            for i in range(1,len(row.contents)):
-                                temp_list.append(row.contents[i].get_text())
-                            scoring_data.append(temp_list)
+        c.execute(self.create_table_queries[0])
 
 
-                except URLError, e:
-                    print 'Error Code'
-
-                count = count + 40
-
-            for i in range(1, len(scoring_data)):
+        scoring_data = [[]]
 
 
-                temp_tuple = (year, scoring_data[i][0],
-                          scoring_data[i][1],scoring_data[i][2],scoring_data[i][3],scoring_data[i][4],scoring_data[i][5]
-                       ,scoring_data[i][6],scoring_data[i][7],scoring_data[i][8])
+        if year != 2017:
+            url = 'http://www.espn.com/mens-college-basketball/statistics/team/_' \
+                  '/stat/scoring-per-game/sort/avgPoints/year/'+str(year)+'/count/' + str(count)
+        else:
+            url = 'http://www.espn.com/mens-college-basketball/statistics/team/_' \
+                  '/stat/scoring-per-game/sort/avgPoints/count/' + str(count)
 
-                c.execute('insert into ScoringData values (?,?,?,?,?,?,?,?,?,?)', temp_tuple )
+        request = Request(url)
 
-            conn.commit()
+        try:
+            response = urlopen(request)
+            soup = BeautifulSoup(response.read(), 'html.parser')
 
-            year -= 1
+            for link in soup.find_all('a'):
+                if 'mens-college-basketball/team/' in link.get('href'):
+                    row = link.find_parent('tr')
+                    temp_list = []
+                    for i in range(1,len(row.contents)):
+                        temp_list.append(row.contents[i].get_text())
+                    scoring_data.append(temp_list)
+
+
+        except URLError, e:
+            print 'Error Code'
+
+
+        for i in range(1, len(scoring_data)):
+
+
+            temp_tuple = (year, scoring_data[i][0],
+                      scoring_data[i][1],scoring_data[i][2],scoring_data[i][3],scoring_data[i][4],scoring_data[i][5]
+                   ,scoring_data[i][6],scoring_data[i][7],scoring_data[i][8])
+
+            c.execute(self.insert_queries[0], temp_tuple )
+
+        conn.commit()
+
+
 
         conn.close()
 
@@ -74,15 +124,6 @@ Instance.get_scores()
 
 
 
-def espnpulls():
-    fulllist = []
-    print 'hello'
-    statslist = ['scoring-per-game','rebounds', 'field-goals', 'free-throws', '3-points', 'assists', 'steals', 'blocks'  ]
-    for year in range(2002,2017):
-        for statitem in statslist:
-            for count in range(1,360,40):
-                fulllist.append('http://www.espn.com/mens-college-basketball/statistics/team/_/stat/'+statitem+'/year/'+ str(year) +'/count/'+str(count))
 
-    print fulllist
-espnpulls()
+
 
